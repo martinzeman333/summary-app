4f900f19cb5a58c1122117dc4197c1ddccfa441e
martinzeman333 <martin@datelier.cz>
2025-03-25 10:11:38 +0100

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    OPENAI_API_KEY: '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', // Nahraď svým skutečným klíčem
+    OPENAI_API_KEY: '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', // Nahraď svým skutečným klíčem
 };
